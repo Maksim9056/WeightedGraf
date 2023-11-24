51f44fcb7f6596a9100f2edd4079f47381abddf1
Maksim9056 <bobreczovm@inbox.ru>
2023-11-24 12:40:27 +0300

    12

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ namespace ConsoleApp15
 {
     public class Graf
     {
+        // Найти крайчайщий путь  от стартортовой ноды до каждой ноды обход в ширину по соседям мы идем в ширину по соседям 
+        // List<string> counter = new List<string>();
+        // private Dictionary<string, List<Tuple<string, double>>> graph = new Dictionary<string, List<Tuple<string, double>>>();
+        //// Use Tuple to store vertex and weight
+
+
         List<string> counter = new List<string>();
 
        // private Dictionary<string, Action<string,double>> graph = new Dictionary<string, Action<string, double>>();
@@ -28,17 +34,19 @@ namespace ConsoleApp15
         }
 
 
-        public void AddEdge(string startVertex, string endVertex, double weight)
+        public void AddEdge(string startVertex, string endVertex, double weight,double weight2)
         {
             if (graph.ContainsKey(startVertex) && graph.ContainsKey(endVertex))
             {
                 if (!graph[startVertex].Any(t => t.Item1 == endVertex) && !graph[endVertex].Any(t => t.Item1 == startVertex))
                 {
                     graph[startVertex].Add(new Tuple<string, double>(endVertex, weight));
-                    graph[endVertex].Add(new Tuple<string, double>(startVertex, weight));
+                    graph[endVertex].Add(new Tuple<string, double>(startVertex, weight2));
                 }
             }
         }
+
+
 
         public void DeleteNode(string node)
         {
@@ -80,6 +88,32 @@ namespace ConsoleApp15
                 }
             }
         }
+
+
+        //public void bfs(string node)
+        //{
+        //    int[] distance = new int[counter.Count];
+        //    string curr_node;
+        //    HashSet<string> visited_bfs = new HashSet<string>();
+        //    Queue<string> queue = new Queue<string>();
+        //    visited_bfs.Add(node);
+        //    queue.Enqueue(node);
+        //    while (queue.Count > 0)
+        //    {
+        //        curr_node = queue.Dequeue();
+        //        foreach (var NextNode in graph[curr_node])
+        //        {
+        //            if (!visited_bfs.Contains(NextNode))
+        //            {
+        //                queue.Enqueue(NextNode);
+        //                visited_bfs.Add(NextNode);
+        //                distance[counter.IndexOf(NextNode)] = distance[counter.IndexOf(curr_node)] + 1;
+        //                Console.WriteLine($"Текущая нода: {curr_node}, Следующая нода: {NextNode}");
+        //                Console.WriteLine(string.Join(", ", distance));
+        //            }
+        //        }
+        //    }
+        //}
         public void dfs(string node, HashSet<string> visited = null, double[] distance = null)
         {
             if (visited == null)
@@ -112,9 +146,13 @@ namespace ConsoleApp15
             }
         }
 
-         public int[,] AdjacencyMatrixCalc()
 
-        {
+        /// <summary>
+        /// Найти крайчайщий путь  от стартортовой ноды до каждой ноды
+        /// </summary>
+        /// <returns></returns>
+         public int[,] AdjacencyMatrixCalc()
+         {
 
             int n = counter.Count;
 
@@ -135,13 +173,61 @@ namespace ConsoleApp15
                     adjacencyMatrix[i, indexToChange] = (int)j.Item2;
 
                 }
-
             }
 
             return adjacencyMatrix;
 
         }
 
+        public void ShortestPathsBFS(string startNode)
+        {
+            Dictionary<string, double> distances = new Dictionary<string, double>();
+            Dictionary<string, string> previousNode = new Dictionary<string, string>();
+
+            foreach (var node in graph.Keys)
+            {
+                distances[node] = double.PositiveInfinity;
+            }
+            distances[startNode] = 0;
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(startNode);
+
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                foreach (var neighbor in graph[currentNode])
+                {
+                    double distanceToNeighbor = distances[currentNode] + neighbor.Item2;
+                    if (distanceToNeighbor < distances[neighbor.Item1])
+                    {
+                        distances[neighbor.Item1] = distanceToNeighbor;
+                        previousNode[neighbor.Item1] = currentNode;
+                        queue.Enqueue(neighbor.Item1);
+                    }
+                }
+            }
+
+            // Выводим кратчайшие пути в консоль
+            foreach (var node in graph.Keys)
+            {
+                if (distances[node] == double.PositiveInfinity)
+                {
+                    Console.WriteLine($"Нет пути от {startNode} до {node}");
+                }
+                else
+                {
+                    Console.Write($"Кратчайший путь от {startNode} до {node}: {startNode}");
+                    var current = node;
+                    while (previousNode.ContainsKey(current))
+                    {
+                        Console.Write($" -> {current}");
+                        current = previousNode[current];
+                    }
+                    Console.WriteLine($", расстояние: {distances[node]}");
+                }
+            }
+        }
 
 
         public static void Main(string[] args)
@@ -163,29 +249,33 @@ namespace ConsoleApp15
             graf.AddNode("South");
             graf.AddNode("Airport");
 
-            //graf.AddEdge("MIT", "Park", 5.0);
-            //graf.AddEdge("Boylston", "Park", 7.5);
-            graf.AddEdge("MIT", "Park",1);
-            graf.AddEdge("Boylston", "Park",2);
-            graf.AddEdge("Downtown", "Park",3);
-            graf.AddEdge("Gov", "Park",4);
-            graf.AddEdge("Boylston", "Tufts",5);
-            graf.AddEdge("Boylston", "Downtown",6);
-            graf.AddEdge("Tufts", "China Town",7);
-            graf.AddEdge("Tufts", "South",8);
-            graf.AddEdge("Gov", "Hay",9);
-            graf.AddEdge("Gov", "State",10);
-            graf.AddEdge("State", "Hay", 11);
-            graf.AddEdge("State", "Downtown", 12);
-            graf.AddEdge("China Town", "Downtown", 13);
-            graf.AddEdge("South", "Downtown", 14);
-            graf.AddEdge("South", "China Town", 15);
-            graf.AddEdge("South", "Airport", 16);
-            graf.AddEdge("State", "Airport", 17);
+        
+            graf.AddEdge("MIT", "Park",1,40);
+            graf.AddEdge("Boylston", "Park",2,43);
+            graf.AddEdge("Downtown", "Park",3,45);
+            graf.AddEdge("Gov", "Park",4,46);
+            graf.AddEdge("Boylston", "Tufts",5,47);
+            graf.AddEdge("Boylston", "Downtown",6,48);
+            graf.AddEdge("Tufts", "China Town",7,49);
+            graf.AddEdge("Tufts", "South",8,50);
+            graf.AddEdge("Gov", "Hay",9,51);
+            graf.AddEdge("Gov", "State",10,52);
+            graf.AddEdge("State", "Hay", 11,53);
+            graf.AddEdge("State", "Downtown", 12,54);
+            graf.AddEdge("China Town", "Downtown", 13,55);
+            graf.AddEdge("South", "Downtown", 14,56);
+            graf.AddEdge("South", "China Town", 15,57);
+            graf.AddEdge("South", "Airport", 16,58);
+            graf.AddEdge("State", "Airport", 17,59);
 
             // другие вызовы AddEdge
             graf.VisualizeGraph();
             graf.dfs("MIT");
+
+
+            Console.WriteLine("Алгоритм ближайщиего пути");
+
+            graf.ShortestPathsBFS("MIT");
 
             Console.WriteLine("Выводим матрицу");
             var arr = graf.AdjacencyMatrixCalc();
@@ -198,8 +288,9 @@ namespace ConsoleApp15
                 Console.WriteLine();
             }
 
-
-            Console.ReadLine();
+            Console.ReadLine();   
+            //graf.AddEdge("MIT", "Park", 5.0);
+            //graf.AddEdge("Boylston", "Park", 7.5);
             //Graf graf = new Graf();
             //graf.AddNode("MIT");
             //graf.AddNode("Park");
